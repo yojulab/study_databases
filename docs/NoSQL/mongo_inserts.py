aa54f2d3877c94ef8174b4cba95f7227dba04a52
Sanghun Oh <otter.oh@gmail.com>
2023-12-13 17:52:33 +0900

    add somethings

--- docs/NoSQL/mongo_inserts.py
+++ docs/NoSQL/mongo_inserts.py
@@ -1,7 +1,18 @@
-# mongodb에 접속
+from pymongo import MongoClient
+# mongodb에 접속 -> 자원에 대한 class
+mongoClient = MongoClient("mongodb://localhost:27017")
 
 # database 연결
+database = mongoClient["local"]
 
 # collection 작업
+collection = database['fruits']
 
 # insert 작업 진행
+collection.insert_one({"name": "키위", 
+                       "color": "갈색", 
+                       "origin": "뉴질랜드"})
+
+dict_fruit = {"name": "오렌지", "color": "주황색", "origin": "미국"}
+collection.insert_one(dict_fruit)
+pass
