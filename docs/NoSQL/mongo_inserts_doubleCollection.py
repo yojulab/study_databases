ac6b06f46a669593cce099752703a518e39ec389
Sanghun Oh <otter.oh@gmail.com>
2023-12-15 17:54:00 +0900

    add

--- docs/NoSQL/mongo_inserts_doubleCollection.py
+++ docs/NoSQL/mongo_inserts_doubleCollection.py
@@ -14,3 +14,37 @@ mixed_fruit = {"name": "오렌지",
               "origin": "미국"}
 result = collection.insert_one(mixed_fruit)
 pass
+
+# 분리 입력 (fruits, fruits_colors)
+# insert fruits 작업 진행
+dict_fruit = {"name": "오렌지", 
+              "origin": "미국"}
+result = collection.insert_one(dict_fruit)
+# insertedId: ObjectId("657bf1225a67627932be6eea")
+print("result.inserted_id : {}".format(result.inserted_id))
+inserted_id = result.inserted_id
+
+# insert fruits_colors 작업 진행
+# [{"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": "주황색"}
+#  , {"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": '갈색'}
+#  , {"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": '노란색'}] 
+
+fruits_colors = [{"color": "주황색"}
+                , {"color": '갈색'}
+                , {"color": '노란색'}] 
+
+list_fruits_colors = list()
+for dict_color in fruits_colors:
+    dict_color["fruits_id"] = inserted_id
+    list_fruits_colors.append(dict_color)
+    pass
+
+# collection fruits_colors
+collection_fruits_colors = database["fruits_colors"]
+
+collection_fruits_colors.insert_many(list_fruits_colors)
+
+# find from fruits_colors
+documents = collection_fruits_colors.find({ "fruits_id" : { "$eq" : inserted_id }})
+
+pass
